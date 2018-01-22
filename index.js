08ea2f31b1634823f00873395c6c3bef4c0f82b9
Paul Le Grand <paul@paullegrand.org>
2018-01-21 19:09:31 -0500

    TODO list

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict'
 
+/**TODO
+-Make the file input dynamic
+-fix the webvtt output so it actually outputs words correctly
+-fix the time formatting
+-Add a check for pronunciation (since it doesn't have start or end time)
+-Probably other stuff too
+*/
+
 const VttConvert = require('./lib/vttConvert')
 const vttConvert = new VttConvert()
 
