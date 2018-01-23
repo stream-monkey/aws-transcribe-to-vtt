2f2d2bca4d81064a8448b0775146b1a1117c0e75
Chris Gerrish <chris@streammonkey.com>
2018-01-23 15:25:22 -0500

    Little cleanup

--- index.js
+++ index.js
@@ -5,31 +5,27 @@
 -Save output to a .vtt file
 -Probably other stuff too
 */
+const fs = require('fs')
 
 const VttConvert = require('./lib/vttConvert')
 const vttConvert = new VttConvert()
 
 
-function runJob(params, callback)
-{
-    vttConvert.convertFile('file', (err, res) =>
+fs.readFile('beemovie.json', 'utf8', (err, file) => {
+    if (err) {
+        console.log(err);
+        return err
+    }
+
+    vttConvert.convertFile(file, (err, res) =>
     {
         if (err)
         {
-            return callback(err)
-        }
-
-            return callback(null, res)
-    })
-
-}
-
-runJob('pathtofile', (err, res) =>
-    {
-        if (err)
-        {
+            console.log(err);
             return err
         }
-
+            console.log(res);
+            process.exit()
             return res
     })
+})
