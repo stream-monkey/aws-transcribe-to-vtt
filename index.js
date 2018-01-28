f2ac76270ad261cf964d7883c45666b9379bb062
Paul Le Grand <paul@paullegrand.org>
2018-01-28 16:07:55 -0500

    Open webdriver page (but it's all hard coded for now... hmm.)

--- index.js
+++ index.js
@@ -6,6 +6,10 @@
 -Probably other stuff too
 */
 const fs = require('fs')
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver'),
+    firefox = require('selenium-webdriver/firefox');
+const async = require('async');
 
 const VttConvert = require('./lib/vttConvert')
 const vttConvert = new VttConvert()
@@ -17,6 +21,19 @@ fs.readFile('beemovie.json', 'utf8', (err, file) => {
         return err
     }
 
+    const {Builder, By, Key, until} = require('selenium-webdriver');
+
+    (async function startPage() {
+        let driver = await new Builder().forBrowser('firefox').build();
+        try {
+            console.log('opening test page...\n');
+            await driver.get('file:///repos/aws-transcribe-to-vtt/index.html');
+        } finally {
+        // await driver.quit();
+            process.exit()
+        }
+    })();
+
     vttConvert.convertFile(file, (err, res) =>
     {
         if (err)
@@ -27,7 +44,6 @@ fs.readFile('beemovie.json', 'utf8', (err, file) => {
         fs.writeFile('output/beemovie.vtt', res, (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
-            process.exit()
         });
         return res
     })
